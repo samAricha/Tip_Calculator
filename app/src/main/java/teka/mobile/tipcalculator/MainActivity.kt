584e65a7626d88df9d397b92e4aa2a8588d879f3
samAricha <samaricha001@gmail.com>
2023-01-06 22:22:13 +0300

    Initial commit

---
+++ app/src/main/java/teka/mobile/tipcalculator/MainActivity.kt
@@ -0,0 +1,63 @@
+package teka.mobile.tipcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import teka.mobile.tipcalculator.ui.theme.TipCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipCalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    DefaultAppPreview()
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultAppPreview() {
+    TipCalculatorTheme {
+        TipCalculatorScreen()
+    }
+}
+
+@Composable
+fun TipCalculatorScreen(){
+    Column (modifier = Modifier.padding(32.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)){
+        Text(text = stringResource(id = R.string.calculate_tip),
+            fontSize = 24.sp,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField()
+
+    }
+}
+
+@Composable
+fun EditNumberField(){
+    TextField(value = "", onValueChange = {})
+}
+
